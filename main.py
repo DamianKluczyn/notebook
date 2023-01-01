68db0c5b25d0092447a7c6cb3438ed3a383f7401
Damian Kluczyński <damiankluczynski00@gmail.com>
2023-01-01 22:52:14 +0100

    Fixed login, register, create note

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class logged_user:
 class database_connection:
     def __init__(self):
         self.con = psycopg2.connect(
-            host='localhost',
             database='note',
             user='postgres',
             password='290e47'
@@ -21,26 +20,27 @@ class hash:
     def __init__(self, password):
         self.password = password.encode('utf-8')
         self.salt = bcrypt.gensalt()
-        self.hashed_password = bcrypt.hashpw(self.password, self.salt)
+        self.hashed_password = bcrypt.hashpw(self.password, self.salt).decode('utf-8')
 
     def get_hashed_password(self):
         return self.hashed_password
 
 class user_exist(database_connection):
     def __init__(self, login):
-        self.login = login
         super().__init__()
+        self.login = login
 
     def check(self):
-        super().cur.execute('select count(*) from "Account" where login = %s', (self.login,))
-        if (super().cur.fetchone() == 0):
+        self.cur.execute('select count(*) from "Account" where login = %s;', (self.login,))
+        in_base = self.cur.fetchone()
+        if (in_base[0] == 1):
             return False
         return True
 
 class register(database_connection):
     def __init__(self):
+        self.login, self.passw, self.fname, self.lname, self.exist = " ", " ", " ", " ", " "
         super().__init__()
-        self.login, self.passw, self.fname, self.lname, self.exist = ""
 
     def register(self):
         self.login = input("\nLogin: ").lower()
@@ -48,11 +48,11 @@ class register(database_connection):
         self.fname = input("First name: ").lower().capitalize()
         self.lname = input("Last name: ").lower().capitalize()
         self.exist = user_exist(self.login)
-        if(not self.exist.check()):
+        if(self.exist.check()):
             self.passw = hash(self.passw)
-            super().cur.execute('insert into "Account" (login, password, fname, lname) values (%s, %s, %s, %s)',
-                         (self.login, self.passw, self.fname, self.lname))
-            super().con.commit()
+            self.cur.execute('insert into "Account" (login, password, fname, lname) values (%s, %s, %s, %s);',
+                         (self.login, self.passw.get_hashed_password(), self.fname, self.lname))
+            self.con.commit()
             print("\nUser registered succesfully!\n")
             return True
         print("\nRegistration failed!\n")
@@ -60,18 +60,19 @@ class register(database_connection):
 
 class login(database_connection):
     def __init__(self):
-        super().__init__()
-        self.login, self.passw, self.exist = ""
-    def login(self):
         self.login = input("\nLogin: ").lower()
         self.passw = input("Password: ")
-        self.exist = user_exist(self.login())
-        if(self.exist.check()):
-            super().cur.execute('select login, password from "Account" where login = %s', (self.login,))
-            rows = super().cur.fetchone()
-            if(bcrypt.checkpw(self.passw.encode('utf-8'),rows[1].encode('utf-8')) == True):
+        self.exist = False
+        super().__init__()
+    def getLogin(self):
+        return self.login
+    def sign(self):
+        self.exist = user_exist(self.login)
+        if(not self.exist.check()):
+            self.cur.execute('select password from "Account" where login = %s;', (self.login,))
+            rows = self.cur.fetchone()
+            if(bcrypt.checkpw(self.passw.encode('utf-8'),rows[0].encode('utf-8')) == True):
                 return True
-            print("Zle haslo!")
         return False
 
 class create_note(database_connection):
@@ -81,15 +82,18 @@ class create_note(database_connection):
         title = input("Title: ")
         content = input("Content: ")
 
-        super().cur.execute('select title, id_account from "Note"')
+        self.cur.execute('select id_account from "Account" where login = %s;',(user,))
+        rows = self.cur.fetchone()
+        id = rows[0]
 
-        rows = super().cur.fetchall()
+        self.cur.execute('select title, id_account from "Note";')
+        rows = self.cur.fetchall()
         for r in rows:
-            if r[0] == title and r[1] == self.id:
+            if r[0] == title and r[1] == id:
                 return False
 
-        super().cur.execute('insert into "Note" (title, content, login) values (%s, %s, %s)', (title, content, user.getlogin()))
-        super().con.commit()
+        self.cur.execute('insert into "Note" (title, content, id_account) values (%s, %s, %s)', (title, content, id))
+        self.con.commit()
         print("Notatka dodana pomyslnie")
         return
 
@@ -97,15 +101,19 @@ class read_note(database_connection):
     def __init__(self):
         super().__init__()
     def read_note(self, user):
-        # Check if any note exist
-        super().cur.execute('select count(*) from "Note" where login = %s', (user.getlogin(),))
-        if (super().cur.fetchone() == 0):
+
+        self.cur.execute('select id_account from "Account" where login = %s;', (user,))
+        rows = self.cur.fetchone()
+        id = rows[0]
+
+        self.cur.execute('select count(*) from "Note" where id_account = %s;', (id,))
+        if (self.cur.fetchone() == 0):
             print("Nie masz zadnej notatki")
             return
 
-        super().cur.execute('select title, content, login from "Note" where login = %s', (user.getlogin(),))
+        self.cur.execute('select title, content, id_account from "Note" where id_account = %s;', (id,))
 
-        rows = super().cur.fetchall()
+        rows = self.cur.fetchall()
         counter = 0
         for r in rows:
             counter += 1
@@ -119,21 +127,21 @@ class read_note(database_connection):
 
         print(f"""\nTitle: {r[0]}\n{r[1]}\n""")
         input("Press Enter to continue...")
-        super().con.commit()
+        self.con.commit()
         return
 
 class update_note(database_connection):
     def __init__(self):
         super().__init__()
     def update_note(self, user):
-        super().cur.execute('select count(*) from "Note" where login = %s', (user.getlogin(),))
-        if (super().cur.fetchone() == 0):
+        self.cur.execute('select count(*) from "Note" where login = %s;', (user,))
+        if (self.cur.fetchone() == 0):
             print("You dont have any notes yet")
             return
 
-        super().cur.execute('select title, content, login, id_note from "Note" where login = %s', (user.getlogin,))
+        self.cur.execute('select title, content, login, id_note from "Note" where login = %s;', (user,))
 
-        rows = super().cur.fetchall()
+        rows = self.cur.fetchall()
         counter = 0
         for r in rows:
             counter += 1
@@ -148,22 +156,22 @@ class update_note(database_connection):
         id_note = rows[choose - 1][3]
         title = input("New title: ")
         content = input("New content: ")
-        super().cur.execute('update "Note" set title = %s, content = %s where id_note = %s', (title, content, id_note))
-        super().con.commit()
+        self.cur.execute('update "Note" set title = %s, content = %s where id_note = %s;', (title, content, id_note))
+        self.con.commit()
         print("Notatka pomyslnie zmieniona")
         return
 class delete_note(database_connection):
     def __init__(self):
         super().__init__()
     def delete_note(self,user):
-        super().cur.execute('select count(*) from "Note" where login = %s', (user.getlogin(),))
-        if (super().cur.fetchone() == 0):
+        self.cur.execute('select count(*) from "Note" where login = %s;', (user,))
+        if (self.cur.fetchone() == 0):
             print("You dont have any notes yet")
             return
 
-        super().cur.execute('select title, content, id_account, id_note from "Note" where login = %s', (user.getlogin(),))
+        self.cur.execute('select title, content, id_account, id_note from "Note" where login = %s;', (user,))
 
-        rows = super().cur.fetchall()
+        rows = self.cur.fetchall()
         counter = 0
         for r in rows:
             counter += 1
@@ -176,8 +184,8 @@ class delete_note(database_connection):
                 break
 
         id_note = rows[choose - 1][3]
-        super().cur.execute('delete from "Note" where id_note = %s', (id_note,))
-        super().con.commit()
+        self.cur.execute('delete from "Note" where id_note = %s;', (id_note,))
+        self.con.commit()
         print("Notatka pomyslnie usunieta")
         return
 class start_menu:
@@ -192,15 +200,17 @@ class start_menu:
             2. Register
             0. Exit
             """))
-            match self.choice:
-                case 1:
-                    register().register()
-                case 2:
-                    log = login().login()
-                    if log:
-                        note_menu(log)
-                case 0:
-                    exit(0)
+            if self.choice == 1:
+                log = login()
+                if log.sign():
+                    note = note_menu(log.getLogin())
+                    note.start()
+                else:
+                    print("Zly login lub haslo!")
+            elif self.choice == 2:
+                register().register()
+            elif self.choice == 0:
+                break
 
 class note_menu:
     def __init__(self, log):
@@ -218,17 +228,20 @@ class note_menu:
             4. Delete
             0. Exit
             """))
-            match self.choice:
-                case 1:
-                    create_note.create_note(self._user)
-                case 2:
-                    read_note.read_note(self._user)
-                case 3:
-                    update_note.update_note(self._user)
-                case 4:
-                    delete_note.delete_note(self._user)
-                case 0:
-                    exit(0)
+            if self.choice == 1:
+                create = create_note()
+                create.create_note(self._user.getlogin())
+            elif self.choice == 2:
+                read = read_note()
+                read.read_note(self._user.getlogin())
+            elif self.choice == 3:
+                update = update_note()
+                update.update_note(self._user.getlogin())
+            elif self.choice == 4:
+                delete = delete_note()
+                delete.delete_note(self._user.getlogin())
+            elif self.choice == 0:
+                break
 
 if __name__ == '__main__':
     start = start_menu()
